3ea9c86eb022079241ed9f193877026aafe5839b
rpr5x <rpr5x@virginia.edu>
2013-09-12 17:52:33 -0400

    Project D2 Commit

--- rpr5x/src/token.h
+++ rpr5x/src/token.h
@@ -1,7 +1,7 @@
 
 /* Tokens.  */
-#define GO 258
-#define TURN 259
+#define MOVE 258
+#define SHIFT 259
 #define VAR 260
 #define JUMP 261
 #define FOR 262
@@ -15,7 +15,7 @@
 #define SQRT 270
 #define FLOAT 271
 #define ID 272
-#define NUMBER 273
+#define INTEGER 273
 #define SEMICOLON 274
 #define PLUS 275
 #define MINUS 276
@@ -24,8 +24,18 @@
 #define OPEN 279
 #define CLOSE 280
 #define ASSIGN 281
-
-
+#define IF 282
+#define THEN 283
+#define ELSE 284
+#define WHILE 285
+#define EQUAL 286
+#define LESS 287
+#define GREATER 288
+#define COMMA 289
+#define PROCEDURE 290
+#define LEFTBRACE 291
+#define RIGHTBRACE 292
+#define ERROR 293
 
 typedef union YYSTYPE
 { int i; node *n; double d;}
