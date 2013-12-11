20d23bc413f12a7d6d4758d4987f2f65f9e0dfa6
rpr5x <rpr5x@virginia.edu>
2013-12-11 11:51:30 -0500

    Project 5 Commit

---
+++ rpr5x/src/pp5v2/tmp.asm
@@ -0,0 +1,364 @@
+	# standard Decaf preamble 
+	  .text
+	  .align 2
+	  .globl main
+  _Stack.Init:
+	# BeginFunc 52
+	  subu $sp, $sp, 8	# decrement sp to make space to save ra, fp
+	  sw $fp, 8($sp)	# save fp
+	  sw $ra, 4($sp)	# save ra
+	  addiu $fp, $sp, 8	# set up new fp
+	  subu $sp, $sp, 52	# decrement sp to make space for locals/temps
+	# _tmp0 = 100
+	  li $t2, 100		# load constant value 100 into $t2
+	  sw $t2, -12($fp)	# spill _tmp0 from $t2 to $fp-12
+	# _tmp1 = 0
+	  li $t2, 0		# load constant value 0 into $t2
+	  sw $t2, -16($fp)	# spill _tmp1 from $t2 to $fp-16
+	# _tmp2 = _tmp0 < _tmp1
+	  lw $t0, -12($fp)	# fill _tmp0 to $t0 from $fp-12
+	  lw $t1, -16($fp)	# fill _tmp1 to $t1 from $fp-16
+	  slt $t2, $t0, $t1	
+	  sw $t2, -20($fp)	# spill _tmp2 from $t2 to $fp-20
+	# IfZ _tmp2 Goto _L0
+	  lw $t0, -20($fp)	# fill _tmp2 to $t0 from $fp-20
+	  beqz $t0, _L0	# branch if _tmp2 is zero 
+	# _tmp3 = "Decaf runtime error: Array size is <= 0\n"
+	  .data			# create string constant marked with label
+	  _string1: .asciiz "Decaf runtime error: Array size is <= 0\n"
+	  .text
+	  la $t2, _string1	# load label
+	  sw $t2, -24($fp)	# spill _tmp3 from $t2 to $fp-24
+	# PushParam _tmp3
+	  subu $sp, $sp, 4	# decrement sp to make space for param
+	  lw $t0, -24($fp)	# fill _tmp3 to $t0 from $fp-24
+	  sw $t0, 4($sp)	# copy param value to stack
+	# LCall _PrintString
+	  jal _PrintString   	# jump to function
+	# PopParams 4
+	  add $sp, $sp, 4	# pop params off stack
+	# LCall _Halt
+	  jal _Halt          	# jump to function
+  _L0:
+	# _tmp4 = 1
+	  li $t2, 1		# load constant value 1 into $t2
+	  sw $t2, -32($fp)	# spill _tmp4 from $t2 to $fp-32
+	# _tmp5 = _tmp4 + _tmp0
+	  lw $t0, -32($fp)	# fill _tmp4 to $t0 from $fp-32
+	  lw $t1, -12($fp)	# fill _tmp0 to $t1 from $fp-12
+	  add $t2, $t0, $t1	
+	  sw $t2, -36($fp)	# spill _tmp5 from $t2 to $fp-36
+	# _tmp6 = 4
+	  li $t2, 4		# load constant value 4 into $t2
+	  sw $t2, -40($fp)	# spill _tmp6 from $t2 to $fp-40
+	# _tmp7 = _tmp5 * _tmp6
+	  lw $t0, -36($fp)	# fill _tmp5 to $t0 from $fp-36
+	  lw $t1, -40($fp)	# fill _tmp6 to $t1 from $fp-40
+	  mul $t2, $t0, $t1	
+	  sw $t2, -44($fp)	# spill _tmp7 from $t2 to $fp-44
+	# PushParam _tmp7
+	  subu $sp, $sp, 4	# decrement sp to make space for param
+	  lw $t0, -44($fp)	# fill _tmp7 to $t0 from $fp-44
+	  sw $t0, 4($sp)	# copy param value to stack
+	# _tmp8 = LCall _Alloc
+	  jal _Alloc         	# jump to function
+	  move $t2, $v0		# copy function return value from $v0
+	  sw $t2, -48($fp)	# spill _tmp8 from $t2 to $fp-48
+	# PopParams 4
+	  add $sp, $sp, 4	# pop params off stack
+	# *(_tmp8) = _tmp0
+	  lw $t0, -12($fp)	# fill _tmp0 to $t0 from $fp-12
+	  lw $t2, -48($fp)	# fill _tmp8 to $t2 from $fp-48
+	  sw $t0, 0($t2) 	# store with offset
+	# _tmp9 = _tmp8 + _tmp6
+	  lw $t0, -48($fp)	# fill _tmp8 to $t0 from $fp-48
+	  lw $t1, -40($fp)	# fill _tmp6 to $t1 from $fp-40
+	  add $t2, $t0, $t1	
+	  sw $t2, -52($fp)	# spill _tmp9 from $t2 to $fp-52
+	# *(this + 8) = _tmp9
+	  lw $t0, -52($fp)	# fill _tmp9 to $t0 from $fp-52
+	  lw $t2, 4($fp)	# fill this to $t2 from $fp+4
+	  sw $t0, 8($t2) 	# store with offset
+	# _tmp10 = 0
+	  li $t2, 0		# load constant value 0 into $t2
+	  sw $t2, -60($fp)	# spill _tmp10 from $t2 to $fp-60
+	# *(this + 4) = _tmp10
+	  lw $t0, -60($fp)	# fill _tmp10 to $t0 from $fp-60
+	  lw $t2, 4($fp)	# fill this to $t2 from $fp+4
+	  sw $t0, 4($t2) 	# store with offset
+	# EndFunc
+	# (below handles reaching end of fn body with no explicit return)
+	  move $sp, $fp		# pop callee frame off stack
+	  lw $ra, -4($fp)	# restore saved ra
+	  lw $fp, 0($fp)	# restore saved fp
+	  jr $ra		# return from function
+  _Stack.Push:
+	# BeginFunc 0
+	  subu $sp, $sp, 8	# decrement sp to make space to save ra, fp
+	  sw $fp, 8($sp)	# save fp
+	  sw $ra, 4($sp)	# save ra
+	  addiu $fp, $sp, 8	# set up new fp
+	# EndFunc
+	# (below handles reaching end of fn body with no explicit return)
+	  move $sp, $fp		# pop callee frame off stack
+	  lw $ra, -4($fp)	# restore saved ra
+	  lw $fp, 0($fp)	# restore saved fp
+	  jr $ra		# return from function
+  _Stack.Pop:
+	# BeginFunc 4
+	  subu $sp, $sp, 8	# decrement sp to make space to save ra, fp
+	  sw $fp, 8($sp)	# save fp
+	  sw $ra, 4($sp)	# save ra
+	  addiu $fp, $sp, 8	# set up new fp
+	  subu $sp, $sp, 4	# decrement sp to make space for locals/temps
+	# EndFunc
+	# (below handles reaching end of fn body with no explicit return)
+	  move $sp, $fp		# pop callee frame off stack
+	  lw $ra, -4($fp)	# restore saved ra
+	  lw $fp, 0($fp)	# restore saved fp
+	  jr $ra		# return from function
+  _Stack.NumElems:
+	# BeginFunc 0
+	  subu $sp, $sp, 8	# decrement sp to make space to save ra, fp
+	  sw $fp, 8($sp)	# save fp
+	  sw $ra, 4($sp)	# save ra
+	  addiu $fp, $sp, 8	# set up new fp
+	# EndFunc
+	# (below handles reaching end of fn body with no explicit return)
+	  move $sp, $fp		# pop callee frame off stack
+	  lw $ra, -4($fp)	# restore saved ra
+	  lw $fp, 0($fp)	# restore saved fp
+	  jr $ra		# return from function
+	# VTable for class Stack
+	  .data
+	  .align 2
+	  Stack:		# label for class Stack vtable
+	  .word _Stack.Init
+	  .word _Stack.Push
+	  .word _Stack.Pop
+	  .word _Stack.NumElems
+	  .text
+  main:
+	# BeginFunc 24
+	  subu $sp, $sp, 8	# decrement sp to make space to save ra, fp
+	  sw $fp, 8($sp)	# save fp
+	  sw $ra, 4($sp)	# save ra
+	  addiu $fp, $sp, 8	# set up new fp
+	  subu $sp, $sp, 24	# decrement sp to make space for locals/temps
+	# _tmp11 = 12
+	  li $t2, 12		# load constant value 12 into $t2
+	  sw $t2, -12($fp)	# spill _tmp11 from $t2 to $fp-12
+	# PushParam _tmp11
+	  subu $sp, $sp, 4	# decrement sp to make space for param
+	  lw $t0, -12($fp)	# fill _tmp11 to $t0 from $fp-12
+	  sw $t0, 4($sp)	# copy param value to stack
+	# _tmp12 = LCall _Alloc
+	  jal _Alloc         	# jump to function
+	  move $t2, $v0		# copy function return value from $v0
+	  sw $t2, -16($fp)	# spill _tmp12 from $t2 to $fp-16
+	# PopParams 4
+	  add $sp, $sp, 4	# pop params off stack
+	# _tmp13 = Stack
+	  la $t2, Stack	# load label
+	  sw $t2, -20($fp)	# spill _tmp13 from $t2 to $fp-20
+	# *(_tmp12) = _tmp13
+	  lw $t0, -20($fp)	# fill _tmp13 to $t0 from $fp-20
+	  lw $t2, -16($fp)	# fill _tmp12 to $t2 from $fp-16
+	  sw $t0, 0($t2) 	# store with offset
+	# s = _tmp12
+	  lw $t2, -16($fp)	# fill _tmp12 to $t2 from $fp-16
+	  sw $t2, -8($fp)	# spill s from $t2 to $fp-8
+	# _tmp14 = *(s)
+	  lw $t0, -8($fp)	# fill s to $t0 from $fp-8
+	  lw $t2, 0($t0) 	# load with offset
+	  sw $t2, -24($fp)	# spill _tmp14 from $t2 to $fp-24
+	# _tmp15 = *(_tmp14)
+	  lw $t0, -24($fp)	# fill _tmp14 to $t0 from $fp-24
+	  lw $t2, 0($t0) 	# load with offset
+	  sw $t2, -28($fp)	# spill _tmp15 from $t2 to $fp-28
+	# PushParam s
+	  subu $sp, $sp, 4	# decrement sp to make space for param
+	  lw $t0, 4($fp)	# fill s to $t0 from $fp+4
+	  sw $t0, 4($sp)	# copy param value to stack
+	# ACall _tmp15
+	  lw $t0, -28($fp)	# fill _tmp15 to $t0 from $fp-28
+	  jalr $t0            	# jump to function
+	# PopParams 4
+	  add $sp, $sp, 4	# pop params off stack
+	# EndFunc
+	# (below handles reaching end of fn body with no explicit return)
+	  move $sp, $fp		# pop callee frame off stack
+	  lw $ra, -4($fp)	# restore saved ra
+	  lw $fp, 0($fp)	# restore saved fp
+	  jr $ra		# return from function
+_PrintInt:
+        subu $sp, $sp, 8
+        sw $fp, 8($sp)
+        sw $ra, 4($sp)
+        addiu $fp, $sp, 8
+        li   $v0, 1
+        lw   $a0, 4($fp)
+        syscall
+        move $sp, $fp
+        lw $ra, -4($fp)
+        lw $fp, 0($fp)
+        jr $ra
+                                
+_PrintString:
+        subu $sp, $sp, 8
+        sw $fp, 8($sp)
+        sw $ra, 4($sp)
+        addiu $fp, $sp, 8
+        li   $v0, 4
+        lw $a0, 4($fp)
+        syscall
+        move $sp, $fp
+        lw $ra, -4($fp)
+        lw $fp, 0($fp)
+        jr $ra
+        
+_PrintBool:
+	subu $sp, $sp, 8
+	sw $fp, 8($sp)
+	sw $ra, 4($sp)
+        addiu $fp, $sp, 8
+	lw $t1, 4($fp)
+	blez $t1, fbr
+	li   $v0, 4		# system call for print_str
+	la   $a0, TRUE		# address of str to print
+	syscall			
+	b end
+fbr:	li   $v0, 4		# system call for print_str
+	la   $a0, FALSE		# address of str to print
+	syscall				
+end:	move $sp, $fp
+	lw $ra, -4($fp)
+	lw $fp, 0($fp)
+	jr $ra
+
+_Alloc:
+        subu $sp, $sp, 8
+        sw $fp, 8($sp)
+        sw $ra, 4($sp)
+        addiu $fp, $sp, 8
+        li   $v0, 9
+        lw $a0, 4($fp)
+	syscall
+        move $sp, $fp
+        lw $ra, -4($fp)
+        lw $fp, 0($fp) 
+        jr $ra
+
+
+_StringEqual:
+	subu $sp, $sp, 8      # decrement sp to make space to save ra, fp
+	sw $fp, 8($sp)        # save fp
+	sw $ra, 4($sp)        # save ra
+	addiu $fp, $sp, 8     # set up new fp
+	subu $sp, $sp, 4      # decrement sp to make space for locals/temps
+
+	li $v0,0
+
+	#Determine length string 1
+	lw $t0, 4($fp)       
+	li $t3,0
+bloop1: 
+	lb $t5, ($t0) 
+	beqz $t5, eloop1	
+	addi $t0, 1
+	addi $t3, 1
+	b bloop1
+eloop1:
+	
+	#Determine length string 2
+	lw $t1, 8($fp)
+	li $t4,0
+bloop2: 
+	lb $t5, ($t1) 
+	beqz $t5, eloop2	
+	addi $t1, 1
+	addi $t4, 1
+	b bloop2
+eloop2:
+	bne $t3,$t4,end1       #Check String Lengths Same
+
+	lw $t0, 4($fp)	
+	lw $t1, 8($fp)
+	li $t3, 0     		
+bloop3:	
+	lb $t5, ($t0) 
+	lb $t6, ($t1) 
+	bne $t5, $t6, end1
+	beqz $t5, eloop3       # if zero, then we hit the end of both strings
+	addi $t3, 1
+	addi $t0, 1
+	addi $t1, 1
+	#bne $t3,$t4,bloop3
+	b bloop3
+eloop3:	li $v0,1
+
+end1:	move $sp, $fp         # pop callee frame off stack
+	lw $ra, -4($fp)       # restore saved ra
+	lw $fp, 0($fp)        # restore saved fp
+	jr $ra                # return from function
+
+_Halt:
+        li $v0, 10
+        syscall
+
+_ReadInteger:
+	subu $sp, $sp, 8      # decrement sp to make space to save ra, fp
+	sw $fp, 8($sp)        # save fp
+	sw $ra, 4($sp)        # save ra
+	addiu $fp, $sp, 8     # set up new fp
+	subu $sp, $sp, 4      # decrement sp to make space for locals/temps
+	li $v0, 5
+	syscall
+	move $sp, $fp         # pop callee frame off stack
+	lw $ra, -4($fp)       # restore saved ra
+	lw $fp, 0($fp)        # restore saved fp
+	jr $ra
+        
+
+_ReadLine:
+	subu $sp, $sp, 8      # decrement sp to make space to save ra, fp
+	sw $fp, 8($sp)        # save fp
+	sw $ra, 4($sp)        # save ra
+	addiu $fp, $sp, 8     # set up new fp
+	subu $sp, $sp, 4      # decrement sp to make space for locals/temps
+	# allocate space to store memory
+	li $a0, 128           # request 128 bytes
+	li $v0, 9	      # syscall "sbrk" for memory allocation
+	syscall               # do the system call
+	# read in the new line
+	li $a1, 128	      # size of the buffer
+	#la $a0, SPACE        
+	move $a0, $v0	      # location of the buffer	
+	li $v0, 8 
+	syscall
+
+	#la $t1, SPACE  
+	move $t1, $a0
+bloop4: 
+	lb $t5, ($t1) 
+	beqz $t5, eloop4	
+	addi $t1, 1
+	b bloop4
+eloop4:
+	addi $t1,-1
+	li $t6,0
+	sb $t6, ($t1)
+
+	#la $v0, SPACE
+	move $v0, $a0	      # save buffer location to v0 as return value	
+	move $sp, $fp         # pop callee frame off stack
+	lw $ra, -4($fp)       # restore saved ra
+	lw $fp, 0($fp)        # restore saved fp
+	jr $ra
+	
+
+	.data
+TRUE:.asciiz "true"
+FALSE:.asciiz "false"
+SPACE:.asciiz "Making Space For Inputed Values Is Fun."
+SPACE2:.asciiz "AAA.\n"
